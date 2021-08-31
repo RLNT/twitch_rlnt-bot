502104c94c19309f2ad6a378579f655e6541d1d4
Relentless <relentless@rlnt.dev>
2021-08-31 18:58:45 +0200

    removed deprecated field from webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -24,7 +24,7 @@ const config: Configuration = {
         rules: [
             {
                 test: /\.ts$/,
-                use: 'ts-loader',
+                loader: 'ts-loader',
                 exclude: /node_modules/
             }
         ]
